a162a038354c0b82d072bacf34f3f076575bc3e8
aalimran <tfthemecafe@gmail.com>
2018-11-13 12:40:25 +0600

    I reduce base color options

--- inc/themeoptions/base-color.php
+++ inc/themeoptions/base-color.php
@@ -7,20 +7,15 @@ add_action( 'customize_register', 'blogstart_customize_register_for_color' );
 
 function blogstart_customize_register_for_color($wp_customize){
 
-	 $wp_customize->add_setting( 'base_text_color', array(
-      'default'   => '',
+	 $wp_customize->add_setting( 'base_color', array(
+      'default'   => '#5cca8d',
       'transport' => 'refresh',
       'sanitize_callback'		=> 'sanitize_hex_color'
     ) );
 
-    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'base_text_color', array(
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'base_color', array(
       'section' => 'colors',
-      'label'   => esc_html__( 'Base Text Color', 'blogstart' ),
+      'label'   => esc_html__( 'Base Color', 'blogstart' ),
     ) ) );
 
-    $wp_customize->add_setting( 'base_background_color', array(
-      'default'   => '',
-      'transport' => 'refresh',
-      'sanitize_callback'   => 'sanitize_hex_color'
-    ) );
 }
